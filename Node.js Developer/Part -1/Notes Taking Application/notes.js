a96c44b20b4cbb742e18399650fb3045a9124222
shanjaybsh <71059513+shanjaybsh@users.noreply.github.com>
2020-09-10 14:49:36 +0530

    Update notes.js

--- Node.js Developer/Part -1/Notes Taking Application/notes.js
+++ Node.js Developer/Part -1/Notes Taking Application/notes.js
@@ -1,72 +1,72 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const addNote = (title, body) => {
+const addNote = (title, body) => {     // Arrow function
     const notes = loadNotes()
-    const duplicateNote = notes.find((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title) // Given title == Existing Title
 
-    if (!duplicateNote) {
-        notes.push({
+    if (!duplicateNote) {   // Given title != existing  title
+        notes.push({         // Then Add Data to JSON file
             title: title,
             body: body
         })
-        saveNotes(notes)
-        console.log(chalk.green.inverse('New note added!'))
+        saveNotes(notes)         
+        console.log(chalk.green.inverse('New note added!'))    // if note add 
     } else {
-        console.log(chalk.red.inverse('Note title taken!'))
+        console.log(chalk.red.inverse('Note title taken!'))    // If Title  == Existing Title
     }
 }
 
-const removeNote = (title) => {
-    const notes = loadNotes()
-    const notesToKeep = notes.filter((note) => note.title !== title)
+const removeNote = (title) => {    // Arrow Function 
+    const notes = loadNotes()      // Check in JSON file
+    const notesToKeep = notes.filter((note) => note.title !== title) 
 
-    if (notes.length > notesToKeep.length) {
+    if (notes.length > notesToKeep.length) {         // Note Title == Title it will removed
         console.log(chalk.green.inverse('Note removed!'))
-        saveNotes(notesToKeep)
-    } else {
-        console.log(chalk.red.inverse('No note found!'))
+        saveNotes(notesToKeep)                       // Remaning Note are saved to JSON file
+    } else {   
+        console.log(chalk.red.inverse('No note found!'))  // Note Title != Title  
     }    
 }
 
-const listNotes = () => {
-    const notes = loadNotes()
+const listNotes = () => {   // Check in the JSON file
+    const notes = loadNotes() 
 
     console.log(chalk.inverse('Your notes'))
 
-    notes.forEach((note) => {
-        console.log(note.title)
+    notes.forEach((note) => {    // Display the Node Title
+        console.log(note.title) 
     })
 }
 
-const readNote = (title) => {
-    const notes = loadNotes()
-    const note = notes.find((note) => note.title === title)
+const readNote = (title) => { 
+    const notes = loadNotes()   // Check Data in JSON File
+    const note = notes.find((note) => note.title === title)  // Given Title == Note Title
 
-    if (note) {
+    if (note) {                // Display the Note Data
         console.log(chalk.inverse(note.title))
         console.log(note.body)
-    } else {
-        console.log(chalk.red.inverse('Note not found!'))
+    } else {                     // Given Title != Note Title
+        console.log(chalk.red.inverse('Note not found :('))         //return  Note not Found
     }
 }
 
-const saveNotes = (notes) => {
-    const dataJSON = JSON.stringify(notes)
-    fs.writeFileSync('notes.json', dataJSON)
+const saveNotes = (notes) => { 
+    const dataJSON = JSON.stringify(notes)   // Convert JavaScript object into JSON stringify
+    fs.writeFileSync('notes.json', dataJSON)    // Create JSON file and Write Note Data 
 }
 
 const loadNotes = () => {
     try {
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = dataBuffer.toString()
-        return JSON.parse(dataJSON)
-    } catch (e) {
-        return []
+        const dataBuffer = fs.readFileSync('notes.json')  // Read Data in JSON file
+        const dataJSON = dataBuffer.toString()     // Convert Buffer Data into String
+        return JSON.parse(dataJSON)   // Convert JSON string into JavaScript Object
+    } catch (e) { 
+        return []   // Return Empty Note Data
     }
 }
 
-module.exports = {
+module.exports = {   // Export data 
     addNote: addNote,
     removeNote: removeNote,
     listNotes: listNotes,
